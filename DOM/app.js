6c4b3732f8008f255a1262a66822ba92dae4a207
Josep Vidal <josepvidal@dataddo.com>
2024-08-28 18:06:43 +0200

    intro to drilling vs query the DOM to modify and adjust html elements

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,5 @@
-console.log(window)
+console.dir(document); 
+/* console.dir will show you the real JS format ofr the windows object */
+//console.log(document) /* will display htm code not as JSNON */
+
+document.body.children[1].children[0].href = "https://google.com"; /* Acess and assign new value the Anchor element via document like and object accesing every nested part from body to paraghpr whihc is 1 element in array to a which is 0 in lasta array */
