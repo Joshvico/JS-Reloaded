cb0e93739d60f2d672f162f98f82f0ee6b06b9a3
Josep Vidal <josepvidal@dataddo.com>
2024-10-15 16:17:03 +0200

    save to update

--- DOM/app.js
+++ DOM/app.js
@@ -8,3 +8,19 @@ function changePragraphText() {
 }
 
 paragraphElement.addEventListener('click',changePragraphText) /* paragraphElement.addEventListener(event to listen, what will happens by poiting a function, ) */
+
+/* TASK to listen to typped chracters in input element */
+ let typedInput = document.querySelector('input')
+
+
+ function detectedTyping(event){
+/* let enteredText= typedInput.value; value is used to acces input value */
+let enteredText= event.target.value;
+/* let enteredText= event.data; ---> it provides every single item of data when consol.le */
+console.log (enteredText) /* every time you type the console.log print what your wrote */
+console.log(event)
+ }
+
+ typedInput.addEventListener('input',detectedTyping); /* input propety applies for eanything done insed the imput element "Key" is not enough */
+
+
